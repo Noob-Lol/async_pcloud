4134958287b507f56d89ea77583215d7b8208b1e
Noob <111202059+Noob-Lol@users.noreply.github.com>
2025-07-19 00:17:49 +0300

    add not_found_ok and debug logging
    +'async with' support

--- src/async_pcloud/api.py
+++ src/async_pcloud/api.py
@@ -1,5 +1,13 @@
-import aiohttp
+import aiohttp, logging, sys
 from async_pcloud.validate import RequiredParameterCheck
+
+logger = logging.getLogger('async_pcloud')
+logger.setLevel(logging.INFO)
+handler = logging.StreamHandler(sys.stderr)
+handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 class PyCloudAsync:
     """Simple async wrapper for PCloud API."""
@@ -8,21 +16,30 @@ class PyCloudAsync:
         "eapi": "https://eapi.pcloud.com/",
     }
 
-    def __init__(self, token, endpoint="api"):
+    def __init__(self, token, endpoint="eapi"):
         self.token = token
         if endpoint not in self.endpoints:
             raise ValueError("Endpoint (%s) not found. Use one of: %s", endpoint, ",".join(self.endpoints.keys()),)
         else:
             self.endpoint = self.endpoints.get(endpoint)
         self.session = None
+
+    async def __aenter__(self):
+        await self.connect()
+        return self
+
+    async def __aexit__(self, exc_type, exc, tb):
+        await self.disconnect()
     
     async def connect(self):
         """Creates a session, must be called before any requests."""
         self.session = aiohttp.ClientSession(base_url=self.endpoint, timeout=aiohttp.ClientTimeout(10))
+        logger.debug("Connected to PCloud API")
 
     async def disconnect(self):
         if not self.session: return
         await self.session.close()
+        logger.debug("Disconnected from PCloud API")
         self.session = None
 
     def change_token(self, token):
@@ -32,16 +49,24 @@ class PyCloudAsync:
         if not path.startswith("/"): path = "/" + path
         return path
     
-    async def _do_request(self, url, auth=True, method = "GET", data = None, params: dict={}, **kwargs) -> dict:
+    def redact_auth(self, data: dict):
+        if 'auth' in data: data['auth'] = '***'
+        return data
+    
+    async def _do_request(self, url, auth=True, method = "GET", data = None, params: dict={}, not_found_ok=False, **kwargs) -> dict:
         if not self.token and auth: raise Exception("PCloud token is missing.")
         if not self.session: raise Exception("Not connected to PCloud API, call connect() first.")
         params.update(kwargs)
         if auth: params['auth'] = self.token
         if params.get('path'): params['path'] = self._fix_path(params['path'])
+        logger.debug(f"Request: {method} {url} {self.redact_auth(params.copy())}")
         async with self.session.request(method, url, data=data, params=params) as response:
             response_json = await response.json()
+            logger.debug(f"Response: {response_json} {response.status} {response.reason}")
             if response_json["result"] != 0:
-                raise Exception(f"Failed to {method} '{url}': code: {response_json['result']}, error: {response_json.get('error', 'Unknown error')}")
+                error = response_json.get('error', 'Unknown error')
+                if not_found_ok and 'not found' in error: return {}
+                raise Exception(f"Failed to {method} '{url}': code: {response_json['result']}, error: {error}")
             return response_json
         
     async def _default_get(self, url, **kwargs):
@@ -57,7 +82,7 @@ class PyCloudAsync:
         return await self._do_request("userinfo", **kwargs)
         
     async def get_pcloud_token(self, email, password, verbose=False):
-        """Logs into pCloud and returns the token."""
+        """Logs into pCloud and returns the token. Also prints it if verbose."""
         response = await self.userinfo(auth=False, params={'getauth': 1, 'username': email, 'password': password})
         token = response['auth']
         if verbose: print(token)
@@ -69,24 +94,19 @@ class PyCloudAsync:
     
     @RequiredParameterCheck(("path", "fileid"))
     async def getfilelink(self, **kwargs) -> str:
-        """Returns a link to the file. Or the file's bytes if download=True."""
+        """Returns a link to the file."""
         file_url = await self._do_request("getfilelink", **kwargs)
+        if not file_url: return ''
         download_url = 'https://' + file_url['hosts'][0] + file_url['path']
         return download_url
     
     async def get_all_links(self, fileid: int):
         return await self._do_request("getfilelink", params={'fileid': fileid})
-
-    async def get_file(self, file: str, folder: str):
-        files = await self.listfolder(path=folder)
-        file_info = next((f for f in files.get('metadata', {}).get('contents', []) if f['name'] == file), None)
-        if not file_info: return None
-        download_url = await self.getfilelink(fileid=file_info['fileid'])
-        return download_url
     
     @RequiredParameterCheck(("path", "fileid"))
     async def download_file(self, **kwargs):
         download_url = await self.getfilelink(**kwargs)
+        if not download_url: return
         return await self._default_get(download_url)
     
     @RequiredParameterCheck(("files", "data"))
